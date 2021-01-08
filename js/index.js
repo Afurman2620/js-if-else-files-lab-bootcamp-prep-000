29cc55e29942241318651d7870fabe12b6f55d36
industrious-tutorial-1057 <artemfm@gmail.com>
2021-01-08 03:30:22 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -4,3 +4,8 @@ var height = "74"
 // Don't worry about this
 module.exports = { name, height
 }
+if (name === "Joe") {
+    console.log("The name is correct")
+}if (name === "Susan") {
+    console.log("The name is correct")
+}
